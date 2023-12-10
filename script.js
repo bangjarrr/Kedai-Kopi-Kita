e8c0debb8d18d239e36499ad1ca65d3d6ed330e1
alfajarjaya <sansanjaya40@gmail.com>
2023-12-10 17:32:47 +0700

    update new

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var menuIcon = document.getElementById('menu-icon')
 var menuList = document.getElementById('menu-list')
+var menuIconX = document.getElementById('menu-iconX')
 
 menuIcon.addEventListener('click', () => {
-    menuIcon.style.transition = 'all';
-    menuIcon.style.transitionDuration = '2s';
-    menuIcon.style.transitionDelay = '1.2s';
+    menuIcon.classList.toggle ('hidden');
+    menuIconX.classList.toggle ('hidden');
     menuList.classList.toggle ('hidden');
+});
+
+menuIconX.addEventListener('click', () => {
+    menuIcon.classList.toggle ('hidden');
+    menuList.classList.toggle ('hidden');
+    menuIconX.classList.toggle ('hidden');
 });
