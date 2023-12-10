3950847a594b6dba98717e30272e84753a504c92
alfajarjaya <sansanjaya40@gmail.com>
2023-12-10 19:49:09 +0700

    update navbar

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ menuIconX.addEventListener('click', () => {
     menuList.classList.toggle ('hidden');
     menuIconX.classList.toggle ('hidden');
 });
+
+$(document).ready(function () {
+    // Menangani peristiwa klik pada tombol "Social Media"
+    $("#sosmed").click(function () {
+        // Mengubah status tampilan dropdown
+        $("#drop-down-sosmed").toggleClass("active");
+    });
+
+    // Menangani peristiwa klik di luar dropdown untuk menutupnya
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.dropdown').length) {
+            // Menutup dropdown jika diklik di luar area dropdown
+            $("#drop-down-sosmed").removeClass("active");
+        }
+    });
+});
