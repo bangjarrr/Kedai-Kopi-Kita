071329d1c398941f006505647114db1b225c5ecb
alfajarjaya <sansanjaya40@gmail.com>
2023-12-11 22:00:09 +0700

    updateJS

--- script.js
+++ script.js
@@ -50,16 +50,21 @@ $(document).ready(function () {
 function sendToWhatsApp() {
     var nomorWhatsApp = '6281615650796';
     var nama = document.getElementById('nama').value;
-    var pesan = document.getElementById('email').value;
+    var email = document.getElementById('email').value;
     var nomorHp = document.getElementById('nomorHp').value;
     var jumlahPesanan = document.getElementById('jumlahPesanan').value;
     var namaPesanan = document.getElementById('namaPesanan').value;
     var alamat = document.getElementById('alamat').value;
 
+    if (!nama || !email || !nomorHp || !jumlahPesanan || !namaPesanan || !alamat) {
+        alert('Harap isi semua form sebelum mengirim pesan.');
+        return;
+    }
+
     // tautan WhatsApp 
     var linkWhatsApp = 'https://wa.me/' + nomorWhatsApp +
                        '?text=Nama:%20' + encodeURIComponent(nama) +
-                       '%0AEmail:%20' + encodeURIComponent(pesan) +
+                       '%0AEmail:%20' + encodeURIComponent(email) +
                        '%0ANomor%20HP:%20' + encodeURIComponent(nomorHp) +
                        '%0AJumlah%20Pesanan:%20' + encodeURIComponent(jumlahPesanan) +
                        '%0ANama%20Pesanan:%20' + encodeURIComponent(namaPesanan) +
