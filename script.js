6ba2651e1edb15a1cbbcd5cbe2e3449202d7c248
alfajarjaya <sansanjaya40@gmail.com>
2023-12-11 12:39:38 +0700

    upKirim

--- script.js
+++ script.js
@@ -42,8 +42,35 @@ $(document).ready(function () {
             // Animasi scroll halus
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, 'easeInOutExpo'); // Sesuaikan kecepatan dan jenis animasi sesuai kebutuhan
+            }, 800, 'easeInOutExpo'); //  kecepatan dan jenis animasi 
         }
     });
 });
+
+function sendToWhatsApp() {
+    var nomorWhatsApp = '6281615650796';
+    var nama = document.getElementById('nama').value;
+    var pesan = document.getElementById('email').value;
+    var nomorHp = document.getElementById('nomorHp').value;
+    var jumlahPesanan = document.getElementById('jumlahPesanan').value;
+    var namaPesanan = document.getElementById('namaPesanan').value;
+    var alamat = document.getElementById('alamat').value;
+
+    // tautan WhatsApp 
+    var linkWhatsApp = 'https://wa.me/' + nomorWhatsApp +
+                       '?text=Nama:%20' + encodeURIComponent(nama) +
+                       '%0AEmail:%20' + encodeURIComponent(pesan) +
+                       '%0ANomor%20HP:%20' + encodeURIComponent(nomorHp) +
+                       '%0AJumlah%20Pesanan:%20' + encodeURIComponent(jumlahPesanan) +
+                       '%0ANama%20Pesanan:%20' + encodeURIComponent(namaPesanan) +
+                       '%0AAlamat:%20' + encodeURIComponent(alamat);
+
+    window.open(linkWhatsApp, '_blank');
+}
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    sendToWhatsApp();
+});
+
 
