73920323864314b89ff9892d6e03e2c4aa743a79
alfajarjaya <sansanjaya40@gmail.com>
2023-12-13 23:36:46 +0700

    UpdateMenuBaru

--- menu/menu.js
+++ menu/menu.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const foodButton = document.getElementById("foodButton");
     const drinkButton = document.getElementById("drinkButton");
+    const orderButton = document.getElementById("orderButton")
     const foodContent = document.getElementById("foodContent");
     const drinkContent = document.getElementById("drinkContent");
+    const orderContent = document.getElementById("orderContent")
     const content = document.getElementById("content");
+    const orderNowButtons = document.querySelectorAll(".button");
 
     // Tampilkan konten makanan secara default
     showFoodContent();
@@ -16,15 +19,73 @@ document.addEventListener("DOMContentLoaded", function() {
         showDrinkContent();
     });
 
+    orderButton.addEventListener("click", function(){
+        showOrderContent();
+    });
+
+    orderNowButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            showOrderContent();
+        });
+    });
+    
+
+
     function showFoodContent() {
         content.style.transform = "translateX(0)";
         foodContent.style.display = "block";
         drinkContent.style.display = "none";
+        orderContent.style.display = "none";
     }
-
+    
     function showDrinkContent() {
         content.style.transform = "translateX(-0%)";
         foodContent.style.display = "none";
         drinkContent.style.display = "block";
+        orderContent.style.display = "none";
     }
+
+    function showOrderContent() {
+        content.style.transform = "translateX(0)";
+        foodContent.style.display = "none";
+        drinkContent.style.display = "none";
+        orderContent.style.display = "block";
+    }
+
 });
+
+// Untuk mengirim ke WhatsaApp
+
+function sendToWhatsApp() {
+    var nomorWhatsApp = '6281615650796';
+    var nama = document.getElementById('nama').value;
+    var email = document.getElementById('email').value;
+    var nomorHp = document.getElementById('nomorHp').value;
+    var jumlahPesanan = document.getElementById('jumlahPesanan').value;
+    var namaPesanan = document.getElementById('namaPesanan').value;
+    var alamat = document.getElementById('alamat').value;
+
+    if (!nama || !email || !nomorHp || !jumlahPesanan || !namaPesanan || !alamat) {
+        alert('Harap isi semua form sebelum mengirim pesan.');
+        return;
+    }
+
+    // tautan WhatsApp 
+    var linkWhatsApp = 'https://wa.me/' + nomorWhatsApp +
+                       '?text=Nama:%20' + encodeURIComponent(nama) +
+                       '%0AEmail:%20' + encodeURIComponent(email) +
+                       '%0ANomor%20HP:%20' + encodeURIComponent(nomorHp) +
+                       '%0AJumlah%20Pesanan:%20' + encodeURIComponent(jumlahPesanan) +
+                       '%0ANama%20Pesanan:%20' + encodeURIComponent(namaPesanan) +
+                       '%0AAlamat:%20' + encodeURIComponent(alamat);
+
+    window.open(linkWhatsApp, '_blank');
+}
+
+var myForm = document.getElementById('myForm');
+myForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    sendToWhatsApp();
+    myForm.reset();
+});
+
--- script.js
+++ script.js
@@ -66,42 +66,6 @@ $(document).ready(function () {
     });
 });
 
-// Untuk mengirim ke WhatsaApp
-
-function sendToWhatsApp() {
-    var nomorWhatsApp = '6281615650796';
-    var nama = document.getElementById('nama').value;
-    var email = document.getElementById('email').value;
-    var nomorHp = document.getElementById('nomorHp').value;
-    var jumlahPesanan = document.getElementById('jumlahPesanan').value;
-    var namaPesanan = document.getElementById('namaPesanan').value;
-    var alamat = document.getElementById('alamat').value;
-
-    if (!nama || !email || !nomorHp || !jumlahPesanan || !namaPesanan || !alamat) {
-        alert('Harap isi semua form sebelum mengirim pesan.');
-        return;
-    }
-
-    // tautan WhatsApp 
-    var linkWhatsApp = 'https://wa.me/' + nomorWhatsApp +
-                       '?text=Nama:%20' + encodeURIComponent(nama) +
-                       '%0AEmail:%20' + encodeURIComponent(email) +
-                       '%0ANomor%20HP:%20' + encodeURIComponent(nomorHp) +
-                       '%0AJumlah%20Pesanan:%20' + encodeURIComponent(jumlahPesanan) +
-                       '%0ANama%20Pesanan:%20' + encodeURIComponent(namaPesanan) +
-                       '%0AAlamat:%20' + encodeURIComponent(alamat);
-
-    window.open(linkWhatsApp, '_blank');
-}
-
-var myForm = document.getElementById('myForm');
-myForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    sendToWhatsApp();
-    myForm.reset();
-});
-
-
 document.addEventListener("DOMContentLoaded", function() {
     // Ambil elemen-elemen yang ingin dianimasikan
     var animatedElements = document.querySelectorAll(".animate__animated");
@@ -111,3 +75,4 @@ document.addEventListener("DOMContentLoaded", function() {
         element.classList.add("animate__fadeInUp");
     });
 });
+
