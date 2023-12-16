d3666c21a22b1414c5970e0ea6e910c50c3b16e1
bangjarrr <alfajjar16@gmail.com>
2023-12-16 22:21:09 +0700

    tambahMenuGeser

--- menu/menu.js
+++ menu/menu.js
@@ -1,3 +1,5 @@
+// MEnu yang di tekan
+
 document.addEventListener("DOMContentLoaded", function() {
     const foodButton = document.getElementById("foodButton");
     const drinkButton = document.getElementById("drinkButton");
@@ -50,6 +52,89 @@ document.addEventListener("DOMContentLoaded", function() {
         orderContent.style.display = "block";
     }
 
+});
+
+// Menu yang di geser
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const content = document.getElementById('content');
+    let currentIndex = 0;
+
+    // Mengatur event listener untuk mendeteksi perubahan slide
+    content.addEventListener('touchstart', handleTouchStart, false);
+    content.addEventListener('touchmove', handleTouchMove, false);
+
+        // Fungsi untuk menangani peristiwa sentuh awal
+    function handleTouchStart(event) {
+        xDown = event.touches[0].clientX; 
+    }
+
+        // Fungsi untuk menangani peristiwa pergeseran
+    function handleTouchMove(event) {
+        if (!xDown) {
+            return;
+        }
+
+        let xUp = event.touches[0].clientX;
+        let xDiff = xDown - xUp;
+
+            // Menentukan arah pergeseran
+        if (xDiff > 0) {
+                // Geser ke kiri
+            currentIndex = (currentIndex + 1) % 3; 
+        } else {
+                // Geser ke kanan
+            currentIndex = (currentIndex - 1 + 3) % 3; 
+        }
+
+            // Menampilkan konten yang sesuai
+        showContent();
+        xDown = null;
+    }
+
+        // Fungsi untuk menampilkan konten yang sesuai berdasarkan indeks
+    function showContent() {
+        const foodContent = document.getElementById('foodContent');
+        const drinkContent = document.getElementById('drinkContent');
+        const orderContent = document.getElementById('orderContent');
+
+        switch (currentIndex) {
+            case 0:
+                showFoodContent();
+                    break;
+                case 1:
+                    showDrinkContent();
+                    break;
+                case 2:
+                    showOrderContent();
+                    break;
+            }
+        }
+
+        // Fungsi untuk menampilkan konten makanan
+    function showFoodContent() {
+        content.style.transform = 'translateY(0)';
+        foodContent.style.display = 'block';
+        drinkContent.style.display = 'none';
+        orderContent.style.display = 'none';
+    }
+
+        // Fungsi untuk menampilkan konten minuman
+    function showDrinkContent() {
+        content.style.transform = 'translateY(0)';
+        foodContent.style.display = 'none';
+        drinkContent.style.display = 'block';
+        orderContent.style.display = 'none';
+    }
+
+        // Fungsi untuk menampilkan konten Order Now
+    function showOrderContent() {
+        content.style.transform = 'translateY(-200%)';
+        foodContent.style.display = 'none';
+        drinkContent.style.display = 'none';
+        orderContent.style.display = 'block';
+    }
 });
 
 // Untuk mengirim ke WhatsaApp
