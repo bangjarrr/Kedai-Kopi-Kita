d8b497c9ee111a1db8de7ea1a4538ba8d7eb83d5
bangjarrr <alfajjar16@gmail.com>
2023-12-16 22:34:50 +0700

    ubahNavbarMenu

--- menu/menu.js
+++ menu/menu.js
@@ -56,86 +56,80 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Menu yang di geser
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const content = document.getElementById('content');
+//     let currentIndex = 0;
 
-document.addEventListener('DOMContentLoaded', function () {
-    const content = document.getElementById('content');
-    let currentIndex = 0;
+//     // Mengatur event listener untuk mendeteksi perubahan slide
+//     content.addEventListener('touchstart', handleTouchStart, false);
+//     content.addEventListener('touchmove', handleTouchMove, false);
 
-    // Mengatur event listener untuk mendeteksi perubahan slide
-    content.addEventListener('touchstart', handleTouchStart, false);
-    content.addEventListener('touchmove', handleTouchMove, false);
+//         // Fungsi untuk menangani peristiwa pergeseran
+//     function handleTouchMove(event) {
+//         if (!xDown) {
+//             return;
+//         }
 
-        // Fungsi untuk menangani peristiwa sentuh awal
-    function handleTouchStart(event) {
-        xDown = event.touches[0].clientX; 
-    }
+//         let xUp = event.touches[0].clientX;
+//         let xDiff = xDown - xUp;
 
-        // Fungsi untuk menangani peristiwa pergeseran
-    function handleTouchMove(event) {
-        if (!xDown) {
-            return;
-        }
+//             // Menentukan arah pergeseran
+//         if (xDiff > 0) {
+//                 // Geser ke kiri
+//             currentIndex = (currentIndex + 1) % 3; 
+//         } else {
+//                 // Geser ke kanan
+//             currentIndex = (currentIndex - 1 + 3) % 3; 
+//         }
 
-        let xUp = event.touches[0].clientX;
-        let xDiff = xDown - xUp;
+//             // Menampilkan konten yang sesuai
+//         showContent();
+//         xDown = null;
+//     }
 
-            // Menentukan arah pergeseran
-        if (xDiff > 0) {
-                // Geser ke kiri
-            currentIndex = (currentIndex + 1) % 3; 
-        } else {
-                // Geser ke kanan
-            currentIndex = (currentIndex - 1 + 3) % 3; 
-        }
+//         // Fungsi untuk menampilkan konten yang sesuai berdasarkan indeks
+//     function showContent() {
+//         const foodContent = document.getElementById('foodContent');
+//         const drinkContent = document.getElementById('drinkContent');
+//         const orderContent = document.getElementById('orderContent');
 
-            // Menampilkan konten yang sesuai
-        showContent();
-        xDown = null;
-    }
+//         switch (currentIndex) {
+//             case 0:
+//                 showFoodContent();
+//                     break;
+//                 case 1:
+//                     showDrinkContent();
+//                     break;
+//                 case 2:
+//                     showOrderContent();
+//                     break;
+//             }
+//         }
 
-        // Fungsi untuk menampilkan konten yang sesuai berdasarkan indeks
-    function showContent() {
-        const foodContent = document.getElementById('foodContent');
-        const drinkContent = document.getElementById('drinkContent');
-        const orderContent = document.getElementById('orderContent');
+//         // Fungsi untuk menampilkan konten makanan
+//     function showFoodContent() {
+//         content.style.transform = 'translateY(0)';
+//         foodContent.style.display = 'block';
+//         drinkContent.style.display = 'none';
+//         orderContent.style.display = 'none';
+//     }
 
-        switch (currentIndex) {
-            case 0:
-                showFoodContent();
-                    break;
-                case 1:
-                    showDrinkContent();
-                    break;
-                case 2:
-                    showOrderContent();
-                    break;
-            }
-        }
+//         // Fungsi untuk menampilkan konten minuman
+//     function showDrinkContent() {
+//         content.style.transform = 'translateY(0)';
+//         foodContent.style.display = 'none';
+//         drinkContent.style.display = 'block';
+//         orderContent.style.display = 'none';
+//     }
 
-        // Fungsi untuk menampilkan konten makanan
-    function showFoodContent() {
-        content.style.transform = 'translateY(0)';
-        foodContent.style.display = 'block';
-        drinkContent.style.display = 'none';
-        orderContent.style.display = 'none';
-    }
-
-        // Fungsi untuk menampilkan konten minuman
-    function showDrinkContent() {
-        content.style.transform = 'translateY(0)';
-        foodContent.style.display = 'none';
-        drinkContent.style.display = 'block';
-        orderContent.style.display = 'none';
-    }
-
-        // Fungsi untuk menampilkan konten Order Now
-    function showOrderContent() {
-        content.style.transform = 'translateY(0)';
-        foodContent.style.display = 'none';
-        drinkContent.style.display = 'none';
-        orderContent.style.display = 'block';
-    }
-});
+//         // Fungsi untuk menampilkan konten Order Now
+//     function showOrderContent() {
+//         content.style.transform = 'translateY(0)';
+//         foodContent.style.display = 'none';
+//         drinkContent.style.display = 'none';
+//         orderContent.style.display = 'block';
+//     }
+// });
 
 // Untuk mengirim ke WhatsaApp
 
@@ -195,41 +189,3 @@ myForm.addEventListener("submit", function(event) {
     sendToWhatsApp();
     myForm.reset();
 });
-
-
-
-// function sendToWhatsApp() {
-//     var nomorWhatsApp = '6281615650796';
-//     var nama = document.getElementById('nama').value;
-//     var email = document.getElementById('email').value;
-//     var nomorHp = document.getElementById('nomorHp').value;
-//     var jumlahPesanan = document.getElementById('jumlahPesanan').value;
-//     var namaPesanan = document.getElementById('namaPesanan').value;
-//     var alamat = document.getElementById('alamat').value;
-//     var keterangan = document.getElementById('keterangan').value;
-
-//     if (!nama || !email || !nomorHp || !jumlahPesanan || !namaPesanan || !alamat || !keterangan) {
-//         alert('Harap isi semua form sebelum mengirim pesan.');
-//         return;
-//     }
-
-//     // tautan WhatsApp 
-//     var linkWhatsApp = 'https://wa.me/' + nomorWhatsApp +
-//                        '?text=Nama : %20' + encodeURIComponent(nama) +
-//                        '%0AEmail : %20' + encodeURIComponent(email) +
-//                        '%0ANomor%20 HP : %20' + encodeURIComponent(nomorHp) +
-//                        '%0AJumlah%20 Pesanan : %20' + encodeURIComponent(jumlahPesanan) +
-//                        '%0ANama%20 Pesanan : %20' + encodeURIComponent(namaPesanan) +
-//                        '%0AAlamat : %20' + encodeURIComponent(alamat) +
-//                        '%0Aketerangan : %20' + encodeURIComponent(keterangan);
-
-//     window.open(linkWhatsApp, '_blank');
-// }
-
-// var myForm = document.getElementById('myForm');
-// myForm.addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     sendToWhatsApp();
-//     myForm.reset();
-// });
-
